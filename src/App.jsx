045eaff282891443ee58ecce85371e56a111fd54
boda213-alaa-mohmad <alaaabdelrhman213@gmail.com>
2025-09-06 10:54:49 +0300

    adding style menu and resize the menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from './header'
+import { Links } from './links'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Links />
     </>
   )
 }
--- src/header.jsx
+++ src/header.jsx
@@ -1,22 +1,57 @@
-import './header.css'
+import { useEffect, useState } from "react";
+import "./header.css";
+import { Links } from "./links";
+
+//which better () => or count++
 
 export function Header() {
-    return (
-        <header className="header">
-            <div className="logo">
-                <h2>Personal Shape</h2>
-            </div>
-            <ul className='listItem'>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Portofilo</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-            <div className='mobile-toggle' id='mobileToggle'>
-                <div className="hamburger"></div>
-                <div className="hamburger"></div>
-                <div className="hamburger"></div>
-            </div>
-        </header>
-    );
+  let [toggleBtn, setToggleBtn] = useState(false);
+  function handleClick() {
+    setToggleBtn(!toggleBtn);
+  }
+
+  function handleResize() {
+    if (this.window.innerWidth > 767) {
+      setToggleBtn(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize); //removing
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <h2>Personal Shape</h2>
+      </div>
+      <ul className="listItem">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Portofilo</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <div onClick={handleClick} className="mobile-toggle" id="mobileToggle">
+        {toggleBtn ? (
+          <span className="close">❌</span>
+        ) : (
+          <>
+            <div className="hamburger"></div>
+            <div className="hamburger"></div>
+            <div className="hamburger"></div>
+          </>
+        )}
+      </div>
+      <Links isOpen={toggleBtn} />
+    </header>
+  );
 }
